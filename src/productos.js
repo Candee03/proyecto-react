18407b49179272928fa804aa8a1ef5dc28e8605a
Candela Alfano <candela.alfano1503@gmail.com>
2023-03-06 23:53:05 -0300

    agregando imagenes

--- src/productos.js
+++ src/productos.js
@@ -5,7 +5,7 @@ const productosData = [
     id:1,
     nombre: 'poncho', 
     precio:5000,
-    img: "img/bufanda.jpg",
+    img: "https://firebasestorage.googleapis.com/v0/b/dollytienda-b36ab.appspot.com/o/IMG_20220808_172346.jpg?alt=media&token=64e0a7a5-f5e8-4471-bbdb-91bd9a8c7f45",
     categoria:2,
     description: 'tejido a mano con lana de excelente calidad'
     },
@@ -13,7 +13,7 @@ const productosData = [
     id:2,
     nombre: 'gorro', 
     precio:500,
-    img: "img/bufanda.jpg",
+    img: 'https://firebasestorage.googleapis.com/v0/b/dollytienda-b36ab.appspot.com/o/IMG-20200818-WA0003.jpg?alt=media&token=900c0c7a-12d2-47ea-8d68-8fe645d42264',
     categoria:1,
     description: 'tejido a mano con lana de excelente calidad'
     },
@@ -21,7 +21,7 @@ const productosData = [
     id:3,
     nombre: 'bufanda', 
     precio:1500,
-    img: bufandaImg,
+    img: 'https://firebasestorage.googleapis.com/v0/b/dollytienda-b36ab.appspot.com/o/bufanda.jpg?alt=media&token=9170999c-2a16-4253-96ad-d728ab34b5ac',
     categoria:1,
     description: 'tejido a mano con lana de excelente calidad'
     },
@@ -29,7 +29,7 @@ const productosData = [
     id:4,
     nombre: 'campera', 
     precio:7500,
-    img: "img/bufanda.jpg",
+    img: "https://firebasestorage.googleapis.com/v0/b/dollytienda-b36ab.appspot.com/o/IMG_20210201_164319.jpg?alt=media&token=1690de7b-4e8a-4f4b-9129-06e65d11eed0",
     categoria:2,
     description: 'tejido a mano con lana de excelente calidad'
     },
@@ -37,7 +37,7 @@ const productosData = [
     id:5,
     nombre: 'chal', 
     precio:4500,
-    img: "img/bufanda.jpg",
+    img: "https://firebasestorage.googleapis.com/v0/b/dollytienda-b36ab.appspot.com/o/IMG_20220731_174028.jpg?alt=media&token=2a83dcbe-90f2-4bd6-9acb-d91c210aed3c",
     categoria:1,
     description: 'tejido a mano con lana de excelente calidad'
     },
@@ -45,31 +45,23 @@ const productosData = [
     id:6,
     nombre: 'remera', 
     precio:10000,
-    img: "img/bufanda.jpg",
+    img: "https://firebasestorage.googleapis.com/v0/b/dollytienda-b36ab.appspot.com/o/IMG_20210116_165509.jpg?alt=media&token=1476b5c9-70ec-415e-94a6-0a399184bb3c",
     categoria:2,
     description: 'tejido a mano con lana de excelente calidad'
     },
     {
     id:7,
-    nombre: 'mitones', 
+    nombre: 'gorrito bebe', 
     precio:1500,
-    img: "img/bufanda.jpg",
-    categoria:1,
-    description: 'tejido a mano con lana de excelente calidad'
-    },
-    {
-    id:8,
-    nombre: 'escarpines', 
-    precio:7500,
-    img: "img/bufanda.jpg",
+    img: "https://firebasestorage.googleapis.com/v0/b/dollytienda-b36ab.appspot.com/o/IMG-20200818-WA0003.jpg?alt=media&token=900c0c7a-12d2-47ea-8d68-8fe645d42264",
     categoria:3,
     description: 'tejido a mano con lana de excelente calidad'
     },
     {
-    id:9,
+    id:8,
     nombre: 'mantita', 
     precio:3500,
-    img: "img/bufanda.jpg",
+    img: "https://firebasestorage.googleapis.com/v0/b/dollytienda-b36ab.appspot.com/o/IMG_20210616_200911.jpg?alt=media&token=e51c5fa9-ac7c-4c61-804f-9a6f7de04d96",
     categoria:3,
     description: 'tejido a mano con lana de excelente calidad'
     }
